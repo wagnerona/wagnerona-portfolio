d226343dcf3cd6c58a9807f19d97d81e8fa1cdd3
wagnerona <wagnerolarco@gmail.com>
2023-03-15 12:14:28 +0000

    Fixed about not being responsive for small screens

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const About = () => {
-    return (
-        <div name='about' className='w-full h-screen bg-[#4A5B61] text-[#212529]'>
-            <div className='flex flex-col justify-center items-center w-full h-full'>
-                <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+  return (
+    <div name='about' className='w-full md:h-screen bg-[#4A5B61] text-[#212529]'>
+
+      {/* Container */}
+      <div className='flex flex-col justify-center items-center w-full h-full'>
+                <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8 pt-[110px] pb-10'>
                     <div className='sm:text-right pb-8 pl-4'>
                         <p className='text-2xl font-bold inline border-b-4 border-red-500'>
                             A bit about me
@@ -13,7 +15,7 @@ const About = () => {
                     </div>
                     <div></div>
                 </div>
-                <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+                <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4 pb-10'>
                     <div className='sm:text-right text-2xl'>
                         <p>
                             I am passionate about turning ideas and user requirements into
@@ -25,23 +27,23 @@ const About = () => {
                     </div>
                     <div>
                         <div>
-                            <p className='text-2xl sm-text-7xl py-4 max-w-[700px] font-bold text-'>Product design
+                            <p className='text-2xl  py-4 max-w-[700px] font-bold text-'>Product design
                             </p>
                             <p className='text-xl sm:text-base py-2 max-w-[700px] text-gray-300'>The main focus relies on user experience and a passion for innovation</p>
                             <Link to='/wagnerona-portfolio/webWork'>Tell me more</Link>
                         </div>
                         <hr className='my-10 border-gray-500' />
                         <div>
-                            <p className='text-2xl sm-text-7xl py-4 max-w-[700px] font-bold text-'>Web Development
+                            <p className='text-2xl  py-4 max-w-[700px] font-bold text-'>Web Development
                             </p>
-                            <p className='text- py-2 max-w-[700px] text-gray-300'>The main focus relies on user experience and a passion for innovation</p>
+                            <p className='text-xl sm:text-base py-2 max-w-[700px] text-gray-300'>The main focus relies on user experience and a passion for innovation</p>
                             <Link to='/wagnerona-portfolio/webWork'>Tell me more</Link>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+    </div>
+  )
 }
 
 export default About
