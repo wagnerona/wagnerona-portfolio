a0921cd47335d5efeb43eca2e7379823544dbb61
Wagner Ona <wagnerolarco@gmail.com>
2023-12-07 17:11:36 +0000

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
         <footer className=" bg-stone-300 text-gray-600 py-4 sticky-footer text-xs sm:text-base">
             <div className="h-[2px] container mx-auto flex justify-between items-center">
                 <div className="flex space-x-4">
-                    <a href={CV} target="_blank" rel="noreferrer" className='flex items-center px-5'>Download CV<MdFileDownload className='ml-3' /></a>
+                    // <a href={CV} target="_blank" rel="noreferrer" className='flex items-center px-5'>Download CV<MdFileDownload className='ml-3' /></a>
                 </div>
                 <p className='px-5'>© 2023 Wagner Ona. All rights reserved.</p>
             </div>
